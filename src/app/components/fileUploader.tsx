e98906d1572220136894c75ffa98791afc3b9086
Elena Bardho <ebardho17@epoka.edu.al>
2025-03-11 11:51:05 +0000

    Reset input value on each click

--- src/app/components/fileUploader.tsx
+++ src/app/components/fileUploader.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button } from "@mui/material";
 
 const MAX_FILE_SIZE = 30 * 1024 ; // 30KB limit
 const ALLOWED_TYPES = ["application/json", ".unsigned"];
 
 const FileUploader = ({ setUnsignedTransactionHex }: { setUnsignedTransactionHex: (hex: string) => void }) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const validateFile = (file: File) => {
     if (!ALLOWED_TYPES.some((type) => file.type === type || file.name.endsWith(type))) {
       alert("Invalid file type. Please upload a .json or .unsigned file.");
@@ -35,12 +36,16 @@ const FileUploader = ({ setUnsignedTransactionHex }: { setUnsignedTransactionHex
       }
     };
     reader.readAsText(file);
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+
   };
 
   return (
     <Button variant="contained" component="label" color="success" sx={{ whiteSpace: "nowrap", px: 3 }}>
       Upload
-      <input type="file" hidden onChange={handleFileUpload} />
+      <input ref={inputRef} type="file" hidden onChange={handleFileUpload} />
     </Button>
   );
 };
