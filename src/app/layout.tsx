76c25f5ecac1d99a9bfd9f80901dc5288f1822c2
Ryan Williams <ryan.williams@intersectmbo.org>
2025-08-22 11:42:42 +0100

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,13 @@ import "@meshsdk/react/styles.css";
 export const metadata: Metadata = {
   title: "CC Toolkit",
   description: "CC Toolkit App",
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({
