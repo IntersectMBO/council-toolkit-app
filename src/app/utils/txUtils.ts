eaf3bcc295f20b29e7b3a1a590f1ab71ab5ff02b
Ryan Williams <ryan.williams@intersectmbo.org>
2025-08-29 14:26:23 +0100

    yh

--- src/app/utils/txUtils.ts
+++ src/app/utils/txUtils.ts
@@ -7,13 +7,13 @@ const NEXT_PUBLIC_REST_IPFS_GATEWAY = (process.env.NEXT_PUBLIC_REST_IPFS_GATEWAY
 
 let cachedGoodGateway: string | null = null;
 
-let gateway = getOnlineIpfsGateway();
+cachedGoodGateway = await getOnlineIpfsGateway();
 
-// export function getIpfsGateway(refresh: boolean = false): Promise<string | null> {
-//   if (cachedGoodGateway) return cachedGoodGateway;
-//   cachedGoodGateway = getOnlineIpfsGateway();
-//   return cachedGoodGateway;
-// }
+export async function getIpfsGateway(refresh: boolean = false): Promise<string | null> {
+  if (cachedGoodGateway) return cachedGoodGateway;
+  cachedGoodGateway = await getOnlineIpfsGateway();
+  return cachedGoodGateway;
+}
 
 /**
  * Decodes a transaction from a hex string to a CardanoSerializationLib Transaction object.
@@ -91,7 +91,7 @@ export async function getOnlineIpfsGateway() {
 
 export const openInNewTab = async (url: string) => {
   // Ensure the URL is absolute
-  // const gateway = await getIpfsGateway();
+  const gateway = await getIpfsGateway() || "ipfs.io/ipfs/";
   const fullUrl =
     url.startsWith("http://") || url.startsWith("https://")
       ? url
@@ -107,7 +107,7 @@ export const getDataHashFromURI = async (anchorURL: string) => {
     console.log("[getDataHashFromURI] Anchor data null");
   }
   if (anchorURL.startsWith("ipfs")) {
-    // const gateway = await getIpfsGateway();
+    const gateway = await getIpfsGateway() || "ipfs.io/ipfs/";
     anchorURL = "https://" + gateway + anchorURL.slice(7);
   }
   const data = await fetch(anchorURL);
