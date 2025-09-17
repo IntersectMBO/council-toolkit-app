88017e4aae1e26d3a752767afa53d197fdb338e3
Elena Bardho <ebardho17@epoka.edu.al>
2025-09-17 14:29:40 +0100

    Add warnings when vote choice stays the same

--- src/components/transaction-inspector/VoteValidationChecks.tsx
+++ src/components/transaction-inspector/VoteValidationChecks.tsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import { TOOLTIP_MESSAGES } from "../../lib/constants/infoMessages";
 import CheckItem from "../shared/validationCheckItem";
 import { VoteValidationState } from "../../types/types";
+import Alert from "@mui/material/Alert";
 
 export const VoteChecks = ({
   isMetadataAnchorValid,
@@ -19,19 +20,23 @@ export const VoteChecks = ({
         />
       </Box>
 
-     
+      {(voteChange.prevState && !voteChange.isVoteChange )&& (
         <Box display="flex" flexDirection="column" gap={2} width="48%">
-
-          <CheckItem
-            label="Is the vote being recast?"
-            tooltip={TOOLTIP_MESSAGES.CORRECT_METADATA_ANCHOR}
-            value={voteChange?.isVoteChange}
-          />
+          <Alert severity="warning" variant="outlined">
+            You’ve already voted on this proposal.
+          </Alert>
+        </Box>
+      )}
+      {voteChange?.isVoteChange && (
+        <Box display="flex" flexDirection="column" gap={2} width="48%"><CheckItem
+          label="Is the vote being recast? "
+          tooltip={TOOLTIP_MESSAGES.CORRECT_METADATA_ANCHOR}
+          value={voteChange?.prevState !== null} />
           <Box sx={{ mt: -1, ml: 4 }}>
             <i>Note: Changing your vote will override your previous vote from {voteChange?.prevState} to {voteChange?.newState}</i>
           </Box>
         </Box>
-    
+      )}
 
     </Box>
   );
